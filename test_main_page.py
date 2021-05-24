dbe12f4a24e96820b02856ab954483b62a6dcb18
GriMary5566 <grimar_nsk@mail.ru>
2021-05-24 13:45:47 +0300

    Restoring order. Removed unnecessary lines and file.

--- test_main_page.py
+++ test_main_page.py
@@ -22,22 +22,12 @@ class TestLoginFromMainPage():
         page.open()
         page.should_be_login_link()
 
-@pytest.mark.new
+
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) 
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)    
-    basket_page.should_no_products_in_basket()
-    basket_page.should_be_text_basket_is_empty()
-
-@pytest.mark.new
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link) 
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)        
     basket_page.should_no_products_in_basket()
     basket_page.should_be_text_basket_is_empty()
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 import pytest
 import time
 
+
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -15,14 +18,22 @@ import time
                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-#    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear" 
-#   link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"    
     page = ProductPage(browser, link) 
     page.open()
     page.add_product_to_basket()    
     page.sheuld_alert_msg_add_product_to_bascet()
     page.sheuld_name_product_in_basket()
     page.sheuld_product_price_in_alert()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link) 
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)        
+    basket_page.should_no_products_in_basket()
+    basket_page.should_be_text_basket_is_empty()
 
 
 @pytest.mark.xfail
@@ -53,6 +64,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -77,8 +89,9 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message_when_opening_product_page()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):        
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link) 
         page.open()
         page.add_product_to_basket()    
